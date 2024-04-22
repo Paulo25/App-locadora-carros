36bdf3642d42189ba2d30e1dfa860acd301d4512
Paulo Vitor <paulo.vitor.cs97@gmail.com>
2024-04-22 19:26:32 -0300

    feat: implementação api webservice rest para o resource locação

--- app/Http/Controllers/LocacaoController.php
+++ app/Http/Controllers/LocacaoController.php
@@ -3,64 +3,101 @@
 namespace App\Http\Controllers;
 
 use App\Models\Locacao;
-use App\Http\Requests\StoreLocacaoRequest;
-use App\Http\Requests\UpdateLocacaoRequest;
+use App\Repositories\LocacaoRepository;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class LocacaoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    private $locacao;
+
+    public function __construct(Locacao $locacao)
     {
-        //
+        $this->locacao = $locacao;
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Display a listing of the resource.
      */
-    public function create()
+    public function index(Request $request): JsonResponse
     {
-        //
+        $locacaoRepository = new LocacaoRepository($this->locacao);
+
+        if ($request->has('filtro')) {
+            $locacaoRepository->filtrar($request->filtro);
+        }
+        if ($request->has('atributos')) {
+            $locacaoRepository->selecionarAtributos($request->atributos);
+        }
+        return response()->json($locacaoRepository->obterResultado(), 200);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreLocacaoRequest $request)
+    public function store(Request $request): JsonResponse
     {
-        //
+        $request->validate($this->locacao->rules());
+        $locacao = $this->locacao->create([
+            'cliente_id'                    => $request->cliente_id,
+            'carro_id'                      => $request->carro_id,
+            'data_inicio_periodo'           => $request->data_inicio_periodo,
+            'data_final_previsto_periodo'   => $request->data_final_previsto_periodo,
+            'data_final_realizado_periodo'  => $request->data_final_realizado_periodo,
+            'valor_diaria'                  => $request->valor_diaria,
+            'km_inicial'                    => $request->km_inicial,
+            'km_final'                      => $request->km_final
+        ]);
+        return response()->json($locacao, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Locacao $locacao)
+    public function show($id): JsonResponse
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Locacao $locacao)
-    {
-        //
+        $locacao = $this->locacao->find($id);
+        if ($locacao === null) {
+            return response()->json(["error" => "O recurso pesquisado não existe"], 404);
+        }
+        return response()->json($locacao, 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateLocacaoRequest $request, Locacao $locacao)
+    public function update(Request $request, $id): JsonResponse
     {
-        //
+        $locacao = $this->locacao->find($id);
+        if ($locacao === null) {
+            return response()->json(["error" => "Não foi possível realizar atualização. O recurso solicitado não existe."], 404);
+        }
+        if ($request->method() === 'PATCH') {
+            $regrasDinamicas = [];
+            foreach ($locacao->rules() as $input => $regra) {
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+            $request->validate($regrasDinamicas);
+        } else {
+            $request->validate($locacao->rules());
+        }
+        $locacao->fill($request->all());
+        $locacao->update();
+        return response()->json($locacao, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Locacao $locacao)
+    public function destroy($id): JsonResponse
     {
-        //
+        $locacao = $this->locacao->find($id);
+        if ($locacao === null) {
+            return response()->json(["error" => "O recurso pesquisado não existe"], 404);
+        }
+        $locacao->delete();
+        return response()->json(["success" => "A locação foi removida com sucesso."], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarroController;
 use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\LocacaoController;
 use App\Http\Controllers\MarcaController;
 use App\Http\Controllers\ModeloController;
 
@@ -26,7 +27,7 @@ Route::get('/', function(){
     return ['teste' => 'ok'];
 });
 
-Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
+Route::apiResource('locacao', LocacaoController::class);
 Route::apiResource('cliente', ClienteController::class);
 Route::apiResource('carro', CarroController::class);
 Route::apiResource('marca', MarcaController::class);
